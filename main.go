88dc2c0699c47228425d471261463636becf696a
Justin Watkinson <justin.watkinson@gmail.com>
2019-11-14 21:56:12 -0600

    Initial Commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func addTwoDigits(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Hello world")
+}
---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_addTwoDigits(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Happy Path",args{a: 1, b: 2}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoDigits(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("addTwoDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
